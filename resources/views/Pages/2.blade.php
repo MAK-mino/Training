a22a4cb5bc1abd83792f72cc1c758c9b8fad1b6b
MahnoorMAK <mahnoor.azeemkamboh@gmail.com>
2026-09-26 20:07:05 +0500

    Flash changes

--- resources/views/Pages/2.blade.php
+++ resources/views/Pages/2.blade.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Apexbooks Accounting</title>
     <!-- Tailwind CSS -->
+
+  <script>
+    (function() {
+        var t = localStorage.getItem('apexbooks-theme');
+        if (t) document.documentElement.setAttribute('data-theme', t);
+    })();
+</script>
+
+
+
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Font Awesome for Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
